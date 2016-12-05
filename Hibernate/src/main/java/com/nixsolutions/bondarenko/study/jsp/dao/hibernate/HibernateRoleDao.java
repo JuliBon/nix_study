1a2761399d60b9bec53e643480c1aa9e301269b0
JuliBon <YuluaBondarenko93@gmail.com>
2016-12-05 19:34:22 +0200

    initial hibarnate

---
+++ Hibernate/src/main/java/com/nixsolutions/bondarenko/study/jsp/dao/hibernate/HibernateRoleDao.java
@@ -0,0 +1,79 @@
+package com.nixsolutions.bondarenko.study.jsp.dao.hibernate;
+
+import com.nixsolutions.bondarenko.study.jsp.HibernateUtil;
+import com.nixsolutions.bondarenko.study.jsp.user.library.Role;
+import com.nixsolutions.bondarenko.study.jsp.dao.RoleDao;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+
+public class HibernateRoleDao implements RoleDao {
+    private SessionFactory sessionFactory;
+
+    public HibernateRoleDao() {
+        sessionFactory = HibernateUtil.getSessioFactory();
+    }
+
+    @Override
+    public void create(Role role)  throws Exception{
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.save(role);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new Exception("Error while creating role", e);
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public void update(Role role)  throws Exception{
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.update(role);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new Exception("Error while updating role", e);
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public void remove(Role role)  throws Exception {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.delete(role);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new Exception("Error while removing role", e);
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Role findByName(String name) throws Exception{
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            Criteria criteria = session.createCriteria(Role.class);
+            return  (Role) criteria.add(Restrictions.eq("name", name))
+                    .uniqueResult();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw new Exception("Error while searching role", e);
+        } finally {
+            session.close();
+        }
+    }
+}
