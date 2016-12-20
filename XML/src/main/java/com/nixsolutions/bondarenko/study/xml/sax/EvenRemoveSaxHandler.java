f27456ad87e641147ba43689193eefc35c0af0ee
JuliBon <YuluaBondarenko93@gmail.com>
2016-12-20 12:26:54 +0200

    sax working on stack

--- XML/src/main/java/com/nixsolutions/bondarenko/study/xml/sax/EvenRemoveSaxHandler.java
+++ XML/src/main/java/com/nixsolutions/bondarenko/study/xml/sax/EvenRemoveSaxHandler.java
@@ -13,9 +13,13 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.*;
+import java.util.Stack;
 
 public class EvenRemoveSaxHandler extends DefaultHandler {
     private XMLStreamWriter out;
+
+    private boolean lastWasStart = false;
+    private Stack<Integer> indexStack = new Stack<>();
 
     public EvenRemoveSaxHandler(String outFileName) throws IOException, XMLStreamException {
         OutputStream outputStream = new FileOutputStream(new File(outFileName));
@@ -42,34 +46,73 @@ public class EvenRemoveSaxHandler extends DefaultHandler {
         }
     }
 
+
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        try {
-            out.writeStartElement(localName);
-            for (int i = 0; i < attributes.getLength(); i++) {
-                out.writeAttribute(attributes.getLocalName(i), attributes.getValue(i));
+        if (lastWasStart) {
+            indexStack.push(1);
+        } else {
+            int index = 0;
+            if (indexStack.size() > 0) {
+                index = indexStack.pop();
             }
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
+            indexStack.push(++index);
+        }
+        lastWasStart = true;
+
+        if (checkIfToWrite()) {
+            try {
+                out.writeStartElement(localName);
+                for (int i = 0; i < attributes.getLength(); i++) {
+                    out.writeAttribute(attributes.getLocalName(i), attributes.getValue(i));
+                }
+            } catch (XMLStreamException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        try {
-            out.writeEndElement();
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
+        if (!lastWasStart) {
+            if (indexStack.size() > 0) {
+                indexStack.pop();
+            }
+        }
+        lastWasStart = false;
+
+        if (checkIfToWrite()) {
+            try {
+                out.writeEndElement();
+            } catch (XMLStreamException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
-        try {
-            out.writeCharacters(new String(ch, start, length));
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
+        if (checkIfToWrite()) {
+            try {
+                out.writeCharacters(new String(ch, start, length));
+            } catch (XMLStreamException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private boolean checkIfToWrite() {
+        int size = indexStack.size();
+        boolean toWrite = true;
+        if (size > 0) {
+            for (int i = size - 1; i >= 0; i--) {
+                toWrite = toWrite && (indexStack.get(i) % 2 != 0);
+                if(!toWrite){
+                    break;
+                }
+            }
+        }
+        return toWrite;
     }
 
 
@@ -86,7 +129,7 @@ public class EvenRemoveSaxHandler extends DefaultHandler {
     }
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, XMLStreamException {
-        String fileName = "goods.xml";
+        String fileName = "goods2.xml";
         String baseDir = "src/main/resources/";
         //InputStream resourceAsStream = ClassLoader.getSystemClassLoader().getResourceAsStream(baseDir + fileName);
 
