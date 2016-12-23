489f998effa6ba7b23cf93ed2534298aa3e4d3b2
JuliBon <YuluaBondarenko93@gmail.com>
2016-12-23 18:46:38 +0200

    Working user registration

--- Struts/src/main/java/com/nixsolutions/bondarenko/study/action/Register.java
+++ Struts/src/main/java/com/nixsolutions/bondarenko/study/action/Register.java
@@ -1,6 +1,10 @@
 package com.nixsolutions.bondarenko.study.action;
 
+import com.nixsolutions.bondarenko.study.dao.RoleDao;
 import com.nixsolutions.bondarenko.study.dao.UserDao;
+import com.nixsolutions.bondarenko.study.entity.User;
+import com.nixsolutions.bondarenko.study.entity.UserLibraryRole;
+import com.nixsolutions.bondarenko.study.model.ModelConvert;
 import com.nixsolutions.bondarenko.study.model.UserModel;
 import com.nixsolutions.bondarenko.study.validation.UserCreateValidator;
 import com.opensymphony.xwork2.ActionSupport;
@@ -11,23 +15,26 @@ import java.util.List;
 import java.util.Map;
 
 public class Register extends ActionSupport implements ModelDriven<UserModel> {
+
     private UserDao userDao;
+    private RoleDao roleDao;
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void setRoleDao(RoleDao roleDao) {
+        this.roleDao = roleDao;
     }
 
     private UserModel userModel = new UserModel();
 
     @Override
     public String execute() throws Exception {
-        UserModel userModel = getUserModel();
+        User user = ModelConvert.convertToUser(userModel);
+        user.setRole(roleDao.findByName(UserLibraryRole.USER.name()));
+        userDao.create(user);
         return SUCCESS;
-    }
-
-    @Override
-    public String input() throws Exception {
-        return INPUT;
     }
 
     @Override
--- Struts/src/main/webapp/WEB-INF/pages/success.jsp
+++ Struts/src/main/webapp/WEB-INF/pages/success.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-    Registration successed!
+    Registration successful!
 </body>
 </html>
